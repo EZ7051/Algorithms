55f3d3bd5af0b466437090395fd8701c4d919bc7
Ejaz Ahamed Shaik <35006207+EZ7051@users.noreply.github.com>
2024-10-27 21:51:52 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (18.37%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,16 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         mp = {}
-        for index, ele in enumerate(nums):
-            if ele in mp:
-                mp[ele].append(index)
-            else:
-                mp[ele] = [index]
 
-        for key, val in mp.items():
-            compliment = target - key
+        for idx, val in enumerate(nums):
+            compliment = target - val
+
             if compliment in mp:
-                if len(mp[compliment]) > 1:
-                    return mp[compliment][0], mp[compliment][1]
-                elif mp[compliment][0] != val[0]:
-                    return val[0], mp[compliment][0]
+                return mp[compliment], idx
+            else:
+                mp[val] = idx
+        
